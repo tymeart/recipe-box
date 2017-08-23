06912f059b7f0c8d725b07e26cc324c52830b0b5
Tiffany Lam <tlam5@sbcglobal.net>
2017-08-23 15:02:58 -0700

    Style buttons in recipe details, add spacing to MainSection content, and change font

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -2,20 +2,28 @@ import React from 'react';
 
 const RecipeDetails = (props) => {
   return (
-    <div>
-      <h2>{props.recipe.title}</h2>
-      <button>Edit</button>
-      <button>Delete</button>
+    <div className="RecipeDetails">
+      <div className="recipe-heading">
+        <h2>{props.recipe.title}</h2>
+        <div>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </div>
 
-      <h3>Ingredients</h3>
-      <ul>
-        {props.recipe.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
-      </ul>
-      
-      <h3>Instructions</h3>
-      <ol>
-        {props.recipe.instructions}
-      </ol>
+      <div className="ingredients">
+        <h3>Ingredients</h3>
+        <ul>
+          {props.recipe.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
+        </ul>
+      </div>
+
+      <div className="instructions">
+        <h3>Instructions</h3>
+        <ol>
+          {props.recipe.instructions}
+        </ol>
+      </div>
     </div>
   );
 }
