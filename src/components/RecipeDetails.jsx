a3d4d205b0a694fef7d0d0d455db6dafd04fba40
Tiffany Lam <tlam5@sbcglobal.net>
2017-08-22 19:44:48 -0700

    Add basic styling to recipe form

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -6,10 +6,12 @@ const RecipeDetails = (props) => {
       <h2>{props.recipe.title}</h2>
       <button>Edit</button>
       <button>Delete</button>
+
       <h3>Ingredients</h3>
       <ul>
         {props.recipe.ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
       </ul>
+      
       <h3>Instructions</h3>
       <ol>
         {props.recipe.instructions}
--- src/components/RecipeForm.jsx
+++ src/components/RecipeForm.jsx
@@ -3,15 +3,21 @@ import React, { Component } from 'react';
 class RecipeForm extends Component {
   render() {
     return (
-      <form>
-        <label htmlFor="title">Recipe Title</label>
-        <input id="title" type="text" />
-        <label htmlFor="ingredients">Ingredients</label>
-        <input id="ingredients" type="text" />
-        <label htmlFor="instructions">Instructions</label>
-        <input id="instructions" type="text" />
-        <button>Save</button>
-      </form>
+      <div>
+        <h2>Add a New Recipe</h2>
+        <form>
+          <label htmlFor="title">Recipe Title</label>
+          <input id="title" type="text" />
+
+          <label htmlFor="ingredients">Ingredients</label>
+          <textarea id="ingredients" />
+
+          <label htmlFor="instructions">Instructions</label>
+          <textarea id="instructions" />
+
+          <button>Save</button>
+        </form>
+      </div>
     );
   }
 }
