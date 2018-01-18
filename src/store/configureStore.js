1137b77ffa185d2e5468c8dfe644434130916b74
Tiffany Lam <tlam5@sbcglobal.net>
2018-01-17 18:02:46 -0800

    Remove semicolon

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,6 +5,6 @@ import thunk from 'redux-thunk';
 export default function configureStore() {
   return createStore(
     rootReducer,
-    applyMiddleware(thunk);
+    applyMiddleware(thunk)
   );
 }
